e6020afe804836892b2c0eb3cefa13f61b54fb4d
doublesilver <korea5410@gmail.com>
2026-01-16 15:13:19 +0900

    fix: Explicitly return 403 in adminMiddleware for AUTH_FORBIDDEN

--- backend/src/middleware/adminMiddleware.js
+++ backend/src/middleware/adminMiddleware.js
@@ -9,7 +9,7 @@ const adminMiddleware = (req, res, next) => {
     // ADMIN_KEY가 환경변수에 설정되어 있지 않으면 보안상 모든 접근 차단
     if (!validAdminKey) {
         logger.error('ADMIN_KEY environment variable is not set! Blocking admin access.');
-        return next(AppError.fromErrorCode(ErrorCodes.AUTH_FORBIDDEN));
+        return next(AppError.fromErrorCode(ErrorCodes.AUTH_FORBIDDEN, 403));
     }
 
     if (!adminKey || adminKey !== validAdminKey) {
@@ -17,7 +17,7 @@ const adminMiddleware = (req, res, next) => {
             ip: req.ip,
             keyProvided: adminKey ? '***' : 'none'
         });
-        return next(AppError.fromErrorCode(ErrorCodes.AUTH_FORBIDDEN));
+        return next(AppError.fromErrorCode(ErrorCodes.AUTH_FORBIDDEN, 403));
     }
 
     next();
